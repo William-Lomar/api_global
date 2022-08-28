de85ca1ff3aeaba84c60387b88d9545442a12418
William <lomarw99@gmail.com>
2022-08-27 21:57:36 -0300

    Add midlewares

--- src/express.ts
+++ src/express.ts
@@ -1,9 +1,14 @@
-import express, { Request, Response } from 'express';
+import express, { json } from 'express';
 import consign from 'consign';
+import helmet from 'helmet';
+import cors from 'cors';
 
 export const app = express();
  
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(json());
 
 consign({cwd:'src'}).include('routers').into(app);
 
